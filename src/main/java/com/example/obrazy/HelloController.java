6e90f8bb0055d3ffd24b4386feb4b1abaa06ad5e
Patrycja <patrycjabiskot@gmail.com>
2025-06-03 12:23:54 +0200

    dodane konturowanie i wielowatkowosc, naprawione zapisywanie plikow

--- src/main/java/com/example/obrazy/HelloController.java
+++ src/main/java/com/example/obrazy/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.obrazy;
 
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -9,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.transform.Rotate;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
@@ -16,14 +16,15 @@ import javafx.util.Duration;
 import javafx.util.Pair;
 import javafx.util.converter.IntegerStringConverter;
 import org.controlsfx.control.Notifications;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.BiConsumer;
 import java.util.function.UnaryOperator;
+import java.util.stream.IntStream;
 
 public class HelloController {
     @FXML private ComboBox<String> operations;
@@ -41,6 +42,8 @@ public class HelloController {
     private Image changedImage;
     private Boolean imageChanged = false;
     private double totalRotation = 0; //do obracania addytywnego
+
+    private static final ForkJoinPool customPool = new ForkJoinPool(4);
 
     @FXML
     public void initialize() {
@@ -71,8 +74,11 @@ public class HelloController {
                 case "Progowanie":
                     thresholdDialog();
                     break;
-                }
 
+                case "Kontur":
+                    contour();
+                    break;
+            }
         }
     }
 
@@ -102,6 +108,7 @@ public class HelloController {
                 // wczytaj nowy
                 originalImage = new Image(file.toURI().toString());
                 originalImageView.setImage(originalImage);
+                imageChanged = false;
 
                 // odblokuj opcje
                 operations.setDisable(false);
@@ -176,6 +183,13 @@ public class HelloController {
             }
         });
 
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == saveButtonType) {
+                return fileNameField.getText();
+            }
+            return null;
+        });
+
         dialog.showAndWait().ifPresent(fileName -> {
             try {
                 File picturesDir = new File(System.getProperty("user.home"), "Desktop");
@@ -191,7 +205,14 @@ public class HelloController {
                 }
 
                 BufferedImage bImage = SwingFXUtils.fromFXImage(changedImageView.getImage(), null);
-                if (ImageIO.write(bImage, "jpg", outputFile)) {
+                BufferedImage rgbImage = new BufferedImage(
+                        bImage.getWidth(),
+                        bImage.getHeight(),
+                        BufferedImage.TYPE_INT_RGB
+                );
+                rgbImage.getGraphics().drawImage(bImage, 0, 0, null);
+
+                if (ImageIO.write(rgbImage, "jpg", outputFile)) {
                     showSuccessToast("Zapisano obraz w pliku " + fileName + ".jpg");
                 } else {
                     showErrorToast("Nie udało się zapisać pliku " + fileName + ".jpg");
@@ -279,7 +300,6 @@ public class HelloController {
         validation.accept(widthField, widthError);
         validation.accept(heightField, heightError);
 
-        // Set result converter
         dialog.setResultConverter(dialogButton -> {
             if (dialogButton == scaleButtonType) {
                 if (widthField.getText().isEmpty()) {
@@ -310,25 +330,25 @@ public class HelloController {
             return null;
         });
 
-        // Handle the result properly
         Optional<Pair<Integer, Integer>> result = dialog.showAndWait();
         result.ifPresent(dimensions -> {
             int newWidth = dimensions.getKey();
             int newHeight = dimensions.getValue();
 
-            System.out.println("originalImage = " + originalImage);
-            System.out.println("changedImage = " + changedImage);
+            Image baseImage = (changedImage != null) ? changedImage : originalImage;
 
-            Image scaledImage;
-            // Scaling logic here
-            if (!imageChanged)
-            {   scaledImage = scaleImage(originalImage, newWidth, newHeight);}
-            else {
-                scaledImage = scaleImage(changedImage, newWidth, newHeight);
-            }
-            changedImageView.setImage(scaledImage);
+            System.out.println("Before scaling:");
+            System.out.println("Width: " + baseImage.getWidth());
+            System.out.println("Height: " + baseImage.getHeight());
+
+            Image scaledImage = scaleImage(baseImage, newWidth, newHeight);
+
+            System.out.println("After scaling:");
+            System.out.println("Width: " + scaledImage.getWidth());
+            System.out.println("Height: " + scaledImage.getHeight());
+
             changedImage = scaledImage;
-            imageChanged = true;
+            changedImageView.setImage(scaledImage);
 
             showSuccessToast("Przeskalowano obraz do " + newWidth + "x" + newHeight + "px");
         });
@@ -409,7 +429,8 @@ public class HelloController {
             BufferedImage bufferedImage = SwingFXUtils.fromFXImage(originalImage, null);
 
             // negatyw
-            for (int y = 0; y < bufferedImage.getHeight(); y++) {
+            customPool.submit(() ->
+                    IntStream.range(0, bufferedImage.getHeight()).parallel().forEach(y -> {
                 for (int x = 0; x < bufferedImage.getWidth(); x++) {
                     int rgb = bufferedImage.getRGB(x, y);
                     int r = 255 - ((rgb >> 16) & 0xFF);
@@ -418,7 +439,7 @@ public class HelloController {
                     int newRgb = (r << 16) | (g << 8) | b;
                     bufferedImage.setRGB(x, y, newRgb);
                 }
-            }
+            })).get();
 
             // updatujemy do wyswietlenia
             changedImage = SwingFXUtils.toFXImage(bufferedImage, null);
@@ -428,7 +449,6 @@ public class HelloController {
             showSuccessToast("Negatyw został wygenerowany pomyślnie!");
         } catch (Exception e) {
             showErrorToast("Nie udało się wykonać negatywu");
-            e.printStackTrace();
         }
     }
 
@@ -474,7 +494,6 @@ public class HelloController {
                 showSuccessToast("Progowanie zostało przeprowadzone pomyślnie!");
             } catch (Exception e) {
                 showErrorToast("Nie udało się wykonać progowania");
-                e.printStackTrace();
             }
         });
     }
@@ -492,14 +511,15 @@ public class HelloController {
             grayImage.getGraphics().drawImage(bufferedImage, 0, 0, null);
 
             // progowanie
-            for (int y = 0; y < grayImage.getHeight(); y++) {
-                for (int x = 0; x < grayImage.getWidth(); x++) {
-                    int rgb = grayImage.getRGB(x, y);
-                    int gray = rgb & 0xFF;
-                    int newValue = gray > threshold ? 255 : 0;
-                    grayImage.setRGB(x, y, (newValue << 16) | (newValue << 8) | newValue);
-                }
-            }
+            customPool.submit(() ->
+                    IntStream.range(0, grayImage.getHeight()).parallel().forEach(y -> {
+                        for (int x = 0; x < grayImage.getWidth(); x++) {
+                            int rgb = grayImage.getRGB(x, y);
+                            int gray = rgb & 0xFF;
+                            int newValue = gray > threshold ? 255 : 0;
+                            grayImage.setRGB(x, y, (newValue << 16) | (newValue << 8) | newValue);
+                        }
+                    })).get();
 
             // Update processed image
             changedImage = SwingFXUtils.toFXImage(grayImage, null);
@@ -508,6 +528,109 @@ public class HelloController {
 
         } catch (Exception e) {
             throw new RuntimeException("Thresholding failed", e);
+        }
+    }
+
+//    private void contour()
+//    {
+//        try {
+//            BufferedImage bufferedImage = SwingFXUtils.fromFXImage(originalImage, null);
+//            BufferedImage grayImage = new BufferedImage(
+//                    bufferedImage.getWidth(),
+//                    bufferedImage.getHeight(),
+//                    BufferedImage.TYPE_BYTE_GRAY
+//            );
+//            grayImage.getGraphics().drawImage(bufferedImage, 0, 0, null);
+//
+//            double[][] sobelX = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+//            double[][] sobelY = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+//
+//            BufferedImage result = new BufferedImage(grayImage.getWidth(), grayImage.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+//
+//            Raster grayRaster = grayImage.getRaster();
+//            WritableRaster resultRaster = result.getRaster();
+//
+//            customPool.submit(() ->
+//                    IntStream.range(1, grayImage.getHeight() - 1).parallel().forEach(y -> {
+//                            for (int x = 1; x < grayImage.getWidth() - 1; x++) {
+//                                int pixelX = 0;
+//                                int pixelY = 0;
+//
+//                                // Apply Sobel operator
+//                                for (int i = -1; i <= 1; i++) {
+//                                    for (int j = -1; j <= 1; j++) {
+//                                        int gray = grayRaster.getSample(x + j, y + i, 0);
+//                                        pixelX += (int) (gray * sobelX[i + 1][j + 1]);
+//                                        pixelY += (int) (gray * sobelY[i + 1][j + 1]);
+//                                    }
+//                                }
+//
+//                                int magnitude = (int) Math.sqrt(pixelX * pixelX + pixelY * pixelY);
+//                                magnitude = Math.min(255, Math.max(0, magnitude)); // Clamp to 0-255
+//
+//                                // Apply threshold to emphasize edges
+//                                int edgeValue = magnitude > 50 ? 255 : 0;
+//                                resultRaster.setSample(x, y, 0, edgeValue);
+//                            }
+//                        })
+//            ).get();
+//
+//            // zmien na javaFX image
+//            WritableImage fxImage = SwingFXUtils.toFXImage(result, null);
+//            changedImage = fxImage;
+//            changedImageView.setImage(fxImage);
+//            imageChanged = true;
+//
+//            showSuccessToast("Konturowanie zostało przeprowadzone pomyślnie!");
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            showErrorToast("Nie udało się wykonać konturowania");
+//        }
+//    }
+
+    private void contour() {
+        try {
+            int width = (int) originalImage.getWidth();
+            int height = (int) originalImage.getHeight();
+
+            WritableImage result = new WritableImage(width, height);
+            PixelReader reader = originalImage.getPixelReader();
+            PixelWriter writer = result.getPixelWriter();
+
+            // Sobel kernels
+            double[][] sobelX = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+            double[][] sobelY = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+            customPool.submit(() -> IntStream.range(1, height - 1).parallel().forEach(y -> {
+                for (int x = 1; x < width - 1; x++) {
+                    double pixelX = 0;
+                    double pixelY = 0;
+
+                    // Apply Sobel operator
+                    for (int i = -1; i <= 1; i++) {
+                        for (int j = -1; j <= 1; j++) {
+                            Color color = reader.getColor(x + j, y + i);
+                            double gray = 0.299 * color.getRed() +
+                                    0.587 * color.getGreen() +
+                                    0.114 * color.getBlue();
+                            pixelX += gray * sobelX[i + 1][j + 1];
+                            pixelY += gray * sobelY[i + 1][j + 1];
+                        }
+                    }
+
+                    double magnitude = Math.sqrt(pixelX * pixelX + pixelY * pixelY);
+                    double edgeValue = magnitude > 0.2 ? 1.0 : 0.0; // Threshold
+                    writer.setColor(x, y, new Color(edgeValue, edgeValue, edgeValue, 1.0));
+                }
+            })).get();
+
+            changedImage = result;
+            changedImageView.setImage(result);
+            imageChanged = true;
+            showSuccessToast("Konturowanie zostało przeprowadzone pomyślnie!");
+        } catch (Exception e) {
+            showErrorToast("Nie udało się wykonać konturowania");
         }
     }
 
